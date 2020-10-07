43656fd0c66e75ede15530f9e88c1b81c9feb4ac
NuzhatProgga <52119902+NuzhatProgga@users.noreply.github.com>
2020-10-07 15:24:47 +0600

    registration page

--- UserManagement/views.py
+++ UserManagement/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from .registrationForm import CreateUserForm
 
 # Create your views here.
 def registration(request):
-    form = UserCreationForm()
+    form = CreateUserForm()
     msg =""
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CreateUserForm(request.POST)
         msg="Invalid Input"
         if form.is_valid():
             form.save()
             msg="Registration Successful!"
+            form = CreateUserForm()
             return redirect('login')
     context = {
         "message":msg,
