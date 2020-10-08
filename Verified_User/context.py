e406b286242dee53da27e1379c782f7245d23a57
Yeasin Kabir Joy <18101020@uap-bd.edu>
2020-10-08 13:48:15 +0600

    verified msg

--- Verified_User/context.py
+++ Verified_User/context.py
@@ -10,17 +10,22 @@ def verifiedUser(request):
         verified_user = get_object_or_404(Verified_User,user=request.user)
     except Exception:
         verified_user=False
-
+        msg="Verify yourself"
     if verified_user != False:
         if verified_user.type =="Student":
             if verified_user.status=="Verified":
                 Student=True
+            else:
+                msg="Wait for your verfication"
         if verified_user.type =="Admin":
             if verified_user.status == "Verified":
                 Admin =True
+            else:
+                msg="Wait for your verfication"
     context = {
         'verified_user': verified_user,
         'student':Student,
-        'admin':Admin
+        'admin':Admin,
+        'verify':msg
     }
     return context
